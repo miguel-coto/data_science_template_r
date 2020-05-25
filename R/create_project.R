32c035679b501fc3f7b8d09da3e60c2c17bdd8ca
Miguel Coto <miguel.cotogarcia@fiserv.com>
2020-05-25 09:53:53 -0600

    Create README fiel

--- R/create_project.R
+++ R/create_project.R
@@ -66,6 +66,8 @@ create_project <-
 		dir.create("src/visualization")
 		textRD <- "
 
+## The directory structure of your new project looks like this.
+
 
 ├── README.md          <- The top-level README for analysts using this project.
 ├── data
@@ -114,11 +116,44 @@ create_project <-
 		dir.create("utilities")
 		dir.create("src")
 
+		textRD <- "
+		
+## The directory structure of your new project looks like this.
+
+├── README.md          <- The top-level README for analysts using this project.
+├── data
+│   ├── external       <- Data from third party sources.
+│   ├── interim        <- Intermediate data that has been transformed.
+│   ├── processed      <- The final, canonical data sets for modeling.
+│   └── raw            <- The original, immutable data dump.
+│
+├── docs               <-  Internal documentation in formats: HTML, LaTeX (for printable PDF versions), ePub, Texinfo, manual pages, plain text
+│
+├── notebooks          <- R-markdowns or Jupyter notebooks. Naming convention is a number (for ordering),
+│                         the creator's initials, and a short `-` delimited description, e.g.
+│                         `1.0-mcg-initial-data-exploration`.
+│
+├── references         <- Data dictionaries, manuals, and all other explanatory materials.
+│
+├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
+│
+├── requirements.txt   <- The requirements file for reproducing the analysis environment.
+│
+├── src                <- Source code for use in this project.
+|
+├── test               <- Scripts to generate the tests for the rest of the code.
+|
+├── graphs             <- Generated graphics and figures to be used in reporting
+|
+└── utilities          <- Common functions and code that can be utilized for the rest of the sripts/notebooks. 
+
+		"
+
 	}
 
   # Create README
-  file.create(“README.md”)
-  fileConn <- file(“README.md”)
+  file.create("README.md")
+  fileConn <- file('README.md')
   writeLines(textRD, fileConn)
   close(fileConn)
 
