8b3c806db6d87bc724780be867cb45987e9569b3
Miguel Coto <miguel.cotogarcia@fiserv.com>
2020-05-26 14:11:02 -0600

    Make README looks pretty

--- R/create_project.R
+++ R/create_project.R
@@ -66,49 +66,51 @@ create_project <-
 		dir.create("src/visualization")
 		textRD <- "
 
-## The directory structure of your new project looks like this.
+# The directory structure of your new project looks like this.
+________________________________________________________________________________________
 
+- README.md: The top-level README for analysts using this project.
+- data
+   - external : Data from third party sources.
+   - interim  : Intermediate data that has been transformed.
+   - processed: The final, canonical data sets for modeling.
+   - raw      : The original, immutable data dump.
 
-├── README.md          <- The top-level README for analysts using this project.
-├── data
-│   ├── external       <- Data from third party sources.
-│   ├── interim        <- Intermediate data that has been transformed.
-│   ├── processed      <- The final, canonical data sets for modeling.
-│   └── raw            <- The original, immutable data dump.
-│
-├── docs               <-  Internal documentation in formats: HTML, LaTeX (for printable PDF versions), ePub, Texinfo, manual pages, plain text
-│
-├── models             <- Trained and serialized models, model predictions, or model summaries
-│
-├── notebooks          <- R-markdowns or Jupyter notebooks. Naming convention is a number (for ordering),
-│                         the creator's initials, and a short `-` delimited description, e.g.
-│                         `1.0-mcg-initial-data-exploration`.
-│
-├── references         <- Data dictionaries, manuals, and all other explanatory materials.
-│
-├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
-│
-├── requirements.txt   <- The requirements file for reproducing the analysis environment.
-│
-├── src                <- Source code for use in this project.
-│   │
-│   ├── data           <- Scripts to download or generate data
-│   │   └── make_dataset.R
-│   │
-│   ├── features       <- Scripts to turn raw data into features for modeling
-│   │   └── build_features.R
-│   │
-│   ├── models         <- Scripts to train models and then use trained models to make
-│   │   │                 predictions
-│   │   ├── predict_model.R
-│   │   └── train_model.R
-│   │
-│   ├── visualization  <- Scripts to create exploratory and results oriented visualizations
-│   |    └── visualize.R
-│   |
-│   └── utilities      <- Common functions and code that can be utilized for the rest of the sripts/notebooks. 
-|
-└── test               <- Scripts to generate the tests for the rest of the code."
+- docs: Internal documentation in formats: HTML, LaTeX (for printable PDF versions), ePub,
+        Texinfo, manual pages, plain text
+
+- models: Trained and serialized models, model predictions, or model summaries
+
+- notebooks: R-markdowns or Jupyter notebooks. Naming convention is a number (for ordering),
+             the creator's initials, and a short `-` delimited description, e.g.
+             `1.0-mcg-initial-data-exploration`.
+
+- references: Data dictionaries, manuals, and all other explanatory materials.
+
+- reports: Generated analysis as HTML, PDF, LaTeX, etc.
+
+- requirements.txt: The requirements file for reproducing the analysis environment.
+
+- src: Source code for use in this project.
+
+  - data: Scripts to download or generate data
+      + make_dataset.R
+
+  - features: Scripts to turn raw data into features for modeling
+      + build_features.R
+
+  - models: Scripts to train models and then use trained models to make predictions
+      + predict_model.R
+      + train_model.R
+
+  - visualization: Scripts to create exploratory and results oriented visualizations
+      + visualize.R
+
+  - utilities: Common functions and code that can be utilized for the rest of the sripts/notebooks.
+
+- test: Scripts to generate the tests for the rest of the code.
+		"
+
 
 	} else {
 		#dir.create("development", recursive = TRUE, showWarnings = FALSE)
@@ -117,35 +119,37 @@ create_project <-
 		dir.create("src")
 
 		textRD <- "
-		
-## The directory structure of your new project looks like this.
 
-├── README.md          <- The top-level README for analysts using this project.
-├── data
-│   ├── external       <- Data from third party sources.
-│   ├── interim        <- Intermediate data that has been transformed.
-│   ├── processed      <- The final, canonical data sets for modeling.
-│   └── raw            <- The original, immutable data dump.
-│
-├── docs               <-  Internal documentation in formats: HTML, LaTeX (for printable PDF versions), ePub, Texinfo, manual pages, plain text
-│
-├── notebooks          <- R-markdowns or Jupyter notebooks. Naming convention is a number (for ordering),
-│                         the creator's initials, and a short `-` delimited description, e.g.
-│                         `1.0-mcg-initial-data-exploration`.
-│
-├── references         <- Data dictionaries, manuals, and all other explanatory materials.
-│
-├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
-│
-├── requirements.txt   <- The requirements file for reproducing the analysis environment.
-│
-├── src                <- Source code for use in this project.
-|
-├── test               <- Scripts to generate the tests for the rest of the code.
-|
-├── graphs             <- Generated graphics and figures to be used in reporting
-|
-└── utilities          <- Common functions and code that can be utilized for the rest of the sripts/notebooks. 
+# The directory structure of your new project looks like this.
+__________________________________________________________________________________________________
+
+- README.md: The top-level README for analysts using this project.
+- data
+    - external : Data from third party sources.
+    - interim  : Intermediate data that has been transformed.
+    - processed: The final, canonical data sets for modeling.
+    - raw      : The original, immutable data dump.
+
+- docs: nternal documentation in formats: HTML, LaTeX (for printable PDF versions), ePub, Texinfo,
+        manual pages, plain text
+
+- notebooks: R-markdowns or Jupyter notebooks. Naming convention is a number (for ordering),
+             the creator's initials, and a short `-` delimited description, e.g.
+             `1.0-mcg-initial-data-exploration`.
+
+- references: Data dictionaries, manuals, and all other explanatory materials.
+
+- reports: Generated analysis as HTML, PDF, LaTeX, etc.
+
+- requirements.txt: The requirements file for reproducing the analysis environment.
+
+- src: Source code for use in this project.
+
+- test: Scripts to generate the tests for the rest of the code.
+
+- graphs: Generated graphics and figures to be used in reporting
+
+- utilities: Common functions and code that can be utilized for the rest of the sripts/notebooks.
 
 		"
 
